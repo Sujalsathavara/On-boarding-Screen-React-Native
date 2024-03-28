0ea2f70d597d1be0a62de4b2afce324fd7fdf3e6
Sujal_Sathavra <sujal.sathavara@triforce.co.in>
2024-03-28 17:09:47 +0530

    first commit

---
+++ navigation/appNavigation.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../Screens/HomeScreen";
+import OnbordingScreensp from "../Screens/OnbordingScreen";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigation() {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(false);
+
+  useEffect(() => {
+    AsyncStorage.getItem("alreadyLaunched").then((value) => {
+      if (value === null) {
+        AsyncStorage.setItem("alreadyLaunched", "true");
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Onbording"
+          options={{ headerShown: false }}
+          component={OnbordingScreensp}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
