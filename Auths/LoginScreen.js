f507c645d211da58ebe4fdc5c365454f154424dd
Sujal_Sathavra <sujal.sathavara@triforce.co.in>
2024-04-09 17:29:19 +0530

    design complated

--- Auths/LoginScreen.js
+++ Auths/LoginScreen.js
@@ -1,17 +1,39 @@
-import React from "react";
-import { Text, View, TouchableOpacity, Image, TextInput } from "react-native";
+import React, { useRef } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import skThem from "../them";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
 import { useNavigation } from "@react-navigation/native";
+import FontAwesome, {
+  SolidIcons,
+  RegularIcons,
+  BrandIcons,
+} from "react-native-fontawesome";
 
 export default function LoginScreen() {
   const navigation = useNavigation();
 
+  const scrollViewRef = useRef();
+
+  const autoScroll = () => {
+    let offset = 0;
+    setInterval(() => {
+      offset += 10;
+      scrollViewRef.current?.scrollTo({ x: 0, y: offset, animated: true });
+    }, 1000);
+  };
+
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex">
-        <View className="flex-row justify-start mb-5">
+        <View className="flex-row justify-start">
           <TouchableOpacity
             onPress={() => {
               navigation.goBack();
@@ -26,63 +48,82 @@ export default function LoginScreen() {
           </TouchableOpacity>
         </View>
 
-        <View className="flex-row justify-center mb-5 ">
-          <Image source={require("../assets/images/logo1.png")} />
+        <View className="flex-row justify-center">
+          <Image
+            source={require("../assets/images/logo1.png")}
+            style={{ width: 200, height: 200 }}
+          />
         </View>
       </SafeAreaView>
-      <View
-        className="flex-1 px-8 pt-8"
+
+      <ScrollView
         style={{
           borderTopLeftRadius: 50,
           borderTopRightRadius: 50,
           backgroundColor: skThem.skButton,
         }}>
-        <View className="form space-y-2">
-          <Text className="text-white ml-2">Email Address</Text>
-          <TextInput
-            className="p-2 bg-gray-100  rounded-2xl mb-2"
-            style={{ color: skThem.skButton }}
-            placeholder="Enter Email Address"
-          />
+        <View
+          className="flex-1 px-8 pt-8"
+          style={{
+            borderTopLeftRadius: 50,
+            borderTopRightRadius: 50,
+            backgroundColor: skThem.skButton,
+          }}>
+          <View className="form space-y-2">
+            <Text className="text-white ml-2">Email Address</Text>
+            <TextInput
+              className="p-2 bg-gray-100  rounded-2xl mb-2"
+              style={{ color: skThem.skButton }}
+              placeholder="Enter Email Address"
+            />
 
-          <Text className="text-white ml-2">Password</Text>
-          <TextInput
-            className="p-2 bg-gray-100 rounded-2xl"
-            style={{ color: skThem.skButton }}
-            secureTextEntry
-            placeholder="Enter Password"
-          />
-          <TouchableOpacity className="flex items-end mb-5">
-            <Text className="text-white">Forgot Password?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            className="py-3 rounded-xl"
-            style={{ backgroundColor: skThem.skpbutton }}>
-            <Text
-              className="font-xl font-bold text-center"
-              style={{ color: skThem.skButton }}>
-              Login
+            <Text className="text-white ml-2">Password</Text>
+            <TextInput
+              className="p-2 bg-gray-100 rounded-2xl"
+              style={{ color: skThem.skButton }}
+              secureTextEntry
+              placeholder="Enter Password"
+            />
+            <TouchableOpacity className="flex items-end mb-2">
+              <Text className="text-white">Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="py-3 rounded-xl"
+              style={{ backgroundColor: skThem.skpbutton }}>
+              <Text
+                className="font-xl font-bold text-center"
+                style={{ color: skThem.skButton }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <Text className="text-xl text-white font-bold text-center py-3">
+            Or
+          </Text>
+          <View className="flex-row justify-center space-x-12">
+            <TouchableOpacity className="p-2 bg-white rounded-2xl">
+              <Image
+                source={require("../assets/images/google.png")}
+                className="w-8 h-8"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity className="p-2 bg-white rounded-2xl">
+              <Image
+                source={require("../assets/images/facebook.png")}
+                className="w-8 h-8"
+              />
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center mt-7">
+            <Text className="text-white font-semibold">
+              Don't have an account?
             </Text>
-          </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
+              <Text className="font-semibold text-yellow-600">Sign Up</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <Text className="text-xl text-white font-bold text-center py-5">
-          Or
-        </Text>
-        <View className="flex-row justify-center space-x-12">
-          <TouchableOpacity className="p-2 bg-white rounded-2xl">
-            <Image
-              source={require("../assets/images/google.png")}
-              className="w-10 h-10"
-            />
-          </TouchableOpacity>
-          <TouchableOpacity className="p-2 bg-white rounded-2xl">
-            <Image
-              source={require("../assets/images/facebook.png")}
-              className="w-10 h-10"
-            />
-          </TouchableOpacity>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 }
